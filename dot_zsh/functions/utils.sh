aa257b67c5812dd7a4b444373590614990f12f27
Kusno Mudiarto <mudiarto@gmail.com>
2024-11-18 05:36:41 -0800

    Added mise config & autocomplete

--- dot_zsh/functions/utils.sh
+++ dot_zsh/functions/utils.sh
@@ -13,3 +13,12 @@ function mcd () {
 # use tlp tlp-rdw powertop to also manage power
 alias batt0="upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E 'state|to\ full|percentage'"
 alias batt1="upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -E 'state|to\ full|percentage'"
+
+
+# assh: smarter ssh client
+# see: https://github.com/moul/assh
+alias assh.build="assh config build --no-automatic-rewrite > ~/.ssh/config"
+alias assh.edit="vi ~/.ssh/assh.yml && assh.build"
+
+# recommended by assh to use this alias so we can rebuild config as needed
+alias ssh="assh wrapper ssh --"
