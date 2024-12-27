dcc3407d9dd8552064c2833b0ca12577b4aed976
Kusno Mudiarto <mudiarto@gmail.com>
2024-12-26 18:18:09 -0800

    Added logitech button for MX750, more hosts

--- dot_zsh/functions/logitech.sh
+++ dot_zsh/functions/logitech.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # run this only if there xinput command and is "Logitech USB Trackball" in the `xinput list` output
-if command -v xinput &> /dev/null && xinput list | grep -q "Logitech USB Trackball"; then
+if command -v xinput &> /dev/null && xinput list | grep -qE "Logitech USB Trackball|Logitech MX750"; then
   # logitech marble mouse settings to be able to scroll - seems to work for ubuntu 22.04
   # from: https://ubuntuforums.org/showthread.php?t=2491926
   # regarding left/right click & natural scroll - use standard mouse settings
